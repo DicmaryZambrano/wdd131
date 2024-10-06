acb1f41eb89379436050b363c071e231082de625
DicmaryZambrano <dicmaryzambrano@outlook.com>
2024-10-06 02:41:48 -0400

    created place page

--- scripts/temples.js
+++ scripts/temples.js
@@ -10,7 +10,23 @@ document.getElementById("lastModified").textContent = `Last modified: ${document
 const menu = document.querySelector('#mobile-menu');
 const menuItems = document.querySelector('.nav-menu');
 
-menu.addEventListener('click', function() {
+menu.addEventListener('click', function () {
   menu.classList.toggle('is-active')
   menuItems.classList.toggle('active');
 });
+
+
+let scores = [100, 72, 83, 94, 88, 87];
+let accumulator = 0;
+let count = 0;
+scores.forEach(score => {
+  if (score > 87) {
+    accumulator += score;
+    count++;
+  }
+});
+if (count > 0) {
+  console.log(accumulator / count);
+} else {
+  console.log("No scores reported.");
+}
